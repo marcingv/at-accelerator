8ea021123d8b9ce326d9ccfa4fb12b4cba508089
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-15 15:40:29 +0200

    Created spinner component.

--- src/app/features/tv-show-table/tv-show-table.component.ts
+++ src/app/features/tv-show-table/tv-show-table.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TvShow } from "@core/models";
+import { LoadingSpinnerComponent } from "@shared/loading-spinner";
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingSpinnerComponent],
   templateUrl: './tv-show-table.component.html',
   styleUrls: ['./tv-show-table.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingSpinnerComponent {
+}
